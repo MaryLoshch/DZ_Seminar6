ea9a470539761085cfb16f8c498ec5bfba4a2d49
мария <oops.mari@mail.ru>
2022-12-14 21:36:42 +0300

    Новое сохранение

---
+++ Zadacha_1Hard/Program.cs
@@ -0,0 +1,38 @@
+﻿
+Console.Write("Введите одно целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] fiblist = MadeFibList(num);
+PrintArray(fiblist);
+
+int[] MadeFibList(int num)
+{
+
+    int[] A = new int[num + 1];
+    for (int i = 0; i <= num; i++) A[i] = Fibonachi(i);
+
+
+    int[] ANFib = new int[num + 1];
+    for (int i = 0; i <= num; i++) ANFib[i] = A[i];
+    for (int i = 0; i <= num; i += 2) ANFib[i] = -ANFib[i];
+
+
+    int[] AFib = new int[2 * num + 1];
+    for (int i = 0; i < num; i++) AFib[i] = ANFib[num - i];
+    for (int i = num + 1; i < AFib.Length; i++) AFib[i] = A[i - num];
+
+    return AFib;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int el in array)
+        Console.Write($"{el,2}\t");
+    Console.WriteLine();
+}
+
+int Fibonachi(int n)
+{
+    if (n == 0 || n == 1) return n;
+
+    return Fibonachi(n - 1) + Fibonachi(n - 2);
+}
---
+++ Zadacha_41/Program.cs
@@ -0,0 +1,34 @@
+﻿void FillArray(int[] _num)
+{
+    for (int i = 0; i < _num.Length; i++)
+    {
+        int a;
+        Console.Write($"Введите элемент [{i}]: ");
+        int.TryParse(Console.ReadLine()!, out a);
+        _num[i] = a;
+    }
+}
+void PrintArray(int[] _num)
+{
+    for (int i = 0; i < _num.Length; i++)
+    {
+        Console.Write(_num[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.Clear();
+int size;
+Console.WriteLine("Введите количество чисел М: ");
+int.TryParse(Console.ReadLine()!, out size);
+
+int[] num = new int[size];
+FillArray(num);
+PrintArray(num);
+int count = 0;
+
+for (int i = 0; i < num.Length; i++)
+    if (num[i] > 0)
+        count++;
+
+Console.WriteLine($"В массиве {num.Length} чисел, чисел больше нуля: {count}");
